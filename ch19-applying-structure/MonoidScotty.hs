8bbb500ceb31f2f051fc3d30a4118e71ff1c6a1a
u0164744 <giocosmiano@gmail.com>
2018-11-17 12:12:01 -0500

    ch19 - applying structure - monoid scotty sample

---
+++ ch19-applying-structure/MonoidScotty.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module MonoidScotty where
+
+import Web.Scotty
+import Data.Monoid (mconcat)
+
+-----------------------------------------------------------------------------------
+-- Haskell web framework inspired by Ruby's Sinatra, using WAI and Warp (Official Repository)
+-- https://github.com/scotty-web/scotty
+-- Prelude> stack install scotty
+--
+-- e.g.
+-- http://localhost:3000
+-- http://localhost:3000/test -> Scotty, test me up!
+-----------------------------------------------------------------------------------
+
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat
+      [ "<h1>Scotty, "
+      , beam
+      , " me up!</h1>" ]
