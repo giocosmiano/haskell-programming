9f7975a7a3d67020c131500cf2af8b2262aff4a6
u0164744 <giocosmiano@gmail.com>
2018-12-21 18:02:14 -0500

    ch21 - traversable - concepts and sample http stuff

--- ch21-traversable/TraversableConcepts.hs
+++ ch21-traversable/TraversableConcepts.hs
@@ -76,4 +76,47 @@ function references
   sequence :: Monad m                         =>   [m a] -> m  [a]
   sequenceA :: (Applicative f, Traversable t) => t (f a) -> f (t a)
 
+-----------------------------------------------------------------------------------
+-- Either
+-----------------------------------------------------------------------------------
+data Either a b = Left a | Right b
+                deriving (Eq, Ord, Show)
+
+instance Functor (Either a) where
+  fmap _ (Left x) = Left x
+  fmap f (Right y) = Right (f y)
+
+instance Applicative (Either e) where
+  pure = Right
+  Left  e <*> _ = Left e
+  Right f <*> r = fmap f r
+
+instance Foldable (Either a) where
+  foldMap _ (Left _)  = mempty
+  foldMap f (Right y) = f y
+
+  foldr _ z (Left _)  = z
+  foldr f z (Right y) = f y z
+
+instance Traversable (Either a) where
+  traverse _ (Left x)  = pure (Left x)
+  traverse f (Right y) = Right <$> f y
+
+-----------------------------------------------------------------------------------
+-- Tuple
+-----------------------------------------------------------------------------------
+instance Functor ((,) a) where
+  fmap f (x,y) = (x, f y)
+
+instance Monoid a => Applicative ((,) a) where
+  pure x = (mempty, x)
+  (u, f) <*> (v, x) = (u `mappend` v, f x)
+
+instance Foldable ((,) a) where
+  foldMap f (_, y) = f y
+  foldr f z (_, y) = f y z
+
+instance Traversable ((,) a) where
+  traverse f (x, y) = (,) x <$> f y
+
 -}
