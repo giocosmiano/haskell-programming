b58b60f288618d9504e6624eff891268016f7f87
u0164744 <giocosmiano@gmail.com>
2018-12-22 14:16:45 -0500

    ch21 - traversable - chapter exercises, with fixes on List monad

--- ch21-traversable/TraversableExercises.hs
+++ ch21-traversable/TraversableExercises.hs
@@ -113,13 +113,13 @@ instance Functor List where
 
 instance Applicative List where
   pure x              = Cons x Nil
-  Nil         <*> _   = Nil
-  _           <*> Nil = Nil
   (Cons f fs) <*> xs  = (fmap f xs) `append` (fs <*> xs)
+  _           <*> _   = Nil
 
 instance Monad List where
-  return = pure
-  (Cons x xs) >>= f = f x `append` flatMap f xs
+  return x = Cons x Nil
+  Nil >>= f = Nil
+  (Cons x xs) >>= f = f x `append` (xs >>= f)
 
 instance Foldable List where
   foldMap _ Nil = mempty
@@ -136,20 +136,13 @@ append :: List a -> List a -> List a
 append Nil ys = ys
 append (Cons x xs) ys = Cons x $ xs `append` ys
 
-fold :: (a -> b -> b) -> b -> List a -> b
-fold _ b Nil = b
-fold f b (Cons h t) = f h (fold f b t)
-
-concat' :: List (List a) -> List a
-concat' = fold append Nil
-
-flatMap :: (a -> List b) -> List a -> List b
-flatMap f xs = concat' $ fmap f xs
-
 instance Arbitrary a => Arbitrary (List a) where
   arbitrary = do
     a <- arbitrary
-    return (Cons a Nil)
+    oneof [return $ Nil,
+           return $ (Cons a Nil),
+           return $ (Cons a (Cons a Nil)),
+           return $ (Cons a (Cons a (Cons a (Cons a (Cons a Nil)))))]
 
 instance Eq a => EqProp (List a) where (=-=) = eq
 
