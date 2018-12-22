18c00c2ec78658eacf500127ad2e2f880b1d2f09
u0164744 <giocosmiano@gmail.com>
2018-12-22 10:25:59 -0500

    ch21 - traversable - tree exercises

---
+++ ch21-traversable/TraversableTreeExercises.hs
@@ -0,0 +1,84 @@
+module TraversableExercises where
+
+import Control.Applicative
+import Data.Monoid
+import Data.Traversable
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+-----------------------------------------------------------------------------------
+data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a)
+            deriving (Eq, Show)
+
+instance Functor Tree where
+  fmap _ Empty = Empty
+  fmap f (Leaf x) = Leaf $ f x
+  fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
+
+{-
+instance Applicative Tree where
+  pure            = Leaf
+  Empty <*> _     = Empty
+  _     <*> Empty = Empty
+  (Node f x r) <*> (Node l x r)  = (fmap f xs) `append` (fs <*> xs)
+
+instance Monad Tree where
+  return = pure
+  (Cons x xs) >>= f = f x `append` flatMap f xs
+-}
+
+instance Foldable Tree where
+  foldMap _ Empty = mempty
+  foldMap f (Leaf x) = f x
+  foldMap f (Node l x r) = (foldMap f l) `mappend` (f x) `mappend` (foldMap f r)
+
+--  foldr _ z Empty = z
+--  foldr f z (Leaf x) = f x z
+--  foldr f z (Node l x r) = f x $ ((foldr f z l) (foldr f z r))
+--  foldr f z (Node l x r) = (foldr f z l) $ (f x) $ foldr f z r
+--  foldr f z (Node l x r) =
+--    let left   = foldr f z l
+--        middle = f x
+--        right  = foldr f z r
+--    in  left $ middle $ right
+
+--  foldr _ z Nil = z
+--  foldr f z (Cons x xs) = f x $ foldr f z xs
+
+instance Traversable Tree where
+  traverse _ Empty = pure Empty
+  traverse f (Leaf x) = Leaf <$> f x
+  traverse f (Node l x r) = Node <$> traverse f l <*> f x <*> traverse f r
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary = do
+    a <- arbitrary
+    oneof [return $ Empty,
+           return $ Leaf a,
+           return $ Node (Leaf a) a (Leaf a)]
+
+instance Eq a => EqProp (Tree a) where (=-=) = eq
+
+-----------------------------------------------------------------------------------
+
+-- search --> haskell applicative function checkers
+-- https://stackoverflow.com/questions/36009335/how-do-i-test-this-applicative-instance-with-checkers-no-instance-for-coarbitr
+
+-- https://begriffs.com/posts/2017-01-14-design-use-quickcheck.html
+
+-- https://hackage.haskell.org/package/checkers
+-- https://hackage.haskell.org/package/checkers-0.4.11/docs/Test-QuickCheck-Classes.html
+
+-- https://github.com/conal/checkers
+-- https://github.com/conal/checkers/blob/master/src/Test/QuickCheck/Classes.hs
+
+-----------------------------------------------------------------------------------
+
+main = do
+
+  putStrLn "\nTesting Applicative, Monad, Traversable : Tree"
+--  quickBatch $ applicative (undefined :: Tree (Int, Double, Char))
+--  quickBatch $ monad (undefined :: Tree (Int, Double, Char))
+  quickBatch $ traversable (undefined :: Tree (Int, Double, [Int]))
+
