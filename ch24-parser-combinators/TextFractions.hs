2ade6b7f5c79c78b2dd8ab9652098965100ce0e0
u0164744 <giovanni.cosmiano@thomsonreuters.com>
2019-01-03 12:39:37 -0500

    ch24 - parser combinators - text fractions

--- ch24-parser-combinators/TextFractions.hs
+++ ch24-parser-combinators/TextFractions.hs
@@ -7,6 +7,10 @@ import Data.Ratio ((%))
 import Text.Trifecta
 
 -----------------------------------------------------------------------------------
+-- see details about `OverloadedStrings`
+-- https://www.schoolofhaskell.com/user/kseo/overloaded-string-literals
+-- https://www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/guide-to-ghc-extensions/basic-syntax-extensions
+-----------------------------------------------------------------------------------
 
 badFraction = "1/0"
 alsoBad = "10"
