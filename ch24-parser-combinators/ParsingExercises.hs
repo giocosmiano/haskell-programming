7381f47fc72f64bfecd54cf9c6cb553732035d65
u0164744 <giocosmiano@gmail.com>
2019-01-04 19:28:16 -0500

    ch24 - parser combinators - parsing exercises

--- ch24-parser-combinators/ParsingExercises.hs
+++ ch24-parser-combinators/ParsingExercises.hs
@@ -4,14 +4,23 @@
 module ParsingExercises where
 
 import Control.Applicative
+import Data.Char
 import Text.Trifecta
 import Text.RawString.QQ
+
+-----------------------------------------------------------------------------------
+-- see for details
+-- http://hackage.haskell.org/package/parsers
+-- http://hackage.haskell.org/package/parsers-0.12.9/docs/Text-Parser-Char.html
+-- http://hackage.haskell.org/package/parsers-0.12.9/docs/Text-Parser-Combinators.html
+-----------------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------------
 -- parser for semantic versions as defined by http://semver.org/
 -- After making a working parser, write an Ord instance for the SemVer type that
 -- obeys the specification outlined on the SemVer website.
 -----------------------------------------------------------------------------------
+
 data NumberOrString = NOSS String
                     | NOSI Integer
                     deriving (Eq, Show, Ord)
@@ -22,31 +31,46 @@ type Patch = Integer
 type Release = [NumberOrString]
 type Metadata = [NumberOrString]
 
-data SemVer = SemVer Major Minor Patch 
+data SemVer = SemVer Major Minor Patch Release Metadata
             deriving (Eq, Show, Ord)
--- data SemVer = SemVer Major Minor Patch Release Metadata
---             deriving (Eq, Show, Ord)
 
 -- e.g.
--- parseString parseSemVer mempty "1.0.0-x.7.z.92"
--- parseString parseSemVer mempty "1.0.0-gamma+002"
--- parseString parseSemVer mempty "1.0.0-beta+oof.sha.41af286"
+-- parseString parseSemVer mempty "2.1.1"                      -> Success (SemVer 2 1 1 [] [])
+-- parseString parseSemVer mempty "1.0.0-x.7.z.92"             -> Success (SemVer 1 0 0 [NOSS "x",NOSI 7,NOSS "z",NOSI 92] [])
+-- parseString parseSemVer mempty "1.0.0-gamma+002"            -> Success (SemVer 1 0 0 [NOSS "gamma"] [NOSI 2])
+-- parseString parseSemVer mempty "1.0.0-beta+oof.sha.41af286" -> Success (SemVer 1 0 0 [NOSS "beta"] [NOSS "oof",NOSS "sha",NOSS "41af286"])
 parseSemVer :: Parser SemVer
 parseSemVer = do
-  major <- integer
-  char '.'
-  minor <- integer
-  char '.'
-  patch <- integer
---   char '-'
---   release <- string
---   skipMany (noneOf alphaNum)
-  return $ SemVer major minor patch
+  major    <- integer
+  _        <- char '.'
+  minor    <- integer
+  _        <- char '.'
+  patch    <- integer
+  _        <- many (oneOf "-")
+  releases <- parseNbrOrStr
+  _        <- many (oneOf "+")
+  metadata <- parseNbrOrStr
+  return $ SemVer major minor patch releases metadata
 
+parseNbrOrStr :: Parser [NumberOrString]
+parseNbrOrStr = sepBy parseAlphaNum (symbol ".")
 
--- Skip end of line and whitespace beyond.
-skipEOL :: Parser ()
-skipEOL = skipMany (oneOf "\n")
+parseAlphaNum :: Parser NumberOrString
+parseAlphaNum = do
+  alpNbr <- some alphaNum
+  let val = if all isDigit alpNbr then (NOSI (read alpNbr :: Integer)) else NOSS alpNbr
+  return val
 
 -----------------------------------------------------------------------------------
+-- parser for positive integer values
+-----------------------------------------------------------------------------------
+
+-- e.g.
+-- parseString parseDigit mempty "123" -> Success '1'
+-- parseString parseDigit mempty "abc" -> Failure (interactive):1:1: error: expected: parseDigit; abc<EOF>
+parseDigit :: Parser Char
+parseDigit = satisfyRange '0' '9'
+
+base10Integer :: Parser Integer
+base10Integer = undefined
 
