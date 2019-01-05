ab47079754b3d3e3accaa68a8d768a202a929f2c
u0164744 <giocosmiano@gmail.com>
2019-01-04 22:22:18 -0500

    ch24 - parser combinators - parsing exercises

--- ch24-parser-combinators/ParsingExercises.hs
+++ ch24-parser-combinators/ParsingExercises.hs
@@ -4,6 +4,7 @@
 module ParsingExercises where
 
 import Control.Applicative
+import Control.Monad
 import Data.Char
 import Text.Trifecta
 import Text.RawString.QQ
@@ -86,8 +87,21 @@ parseAlphaNum = do
 -- parseString parseDigit mempty "123" -> Success '1'
 -- parseString parseDigit mempty "abc" -> Failure (interactive):1:1: error: expected: parseDigit; abc<EOF>
 parseDigit :: Parser Char
-parseDigit = satisfyRange '0' '9'
+parseDigit = oneOf "0123456789" <?> "not an integer"
 
+-- parseString base10Integer mempty "123abc" -> Success '123'
+-- parseString base10Integer mempty "abc"    ->
+-- Failure (interactive):1:1: error: expected: parseDigit; abc<EOF>
 base10Integer :: Parser Integer
-base10Integer = undefined
+base10Integer = read <$> some parseDigit
 
+-- parseString base10Integer' mempty "-123abc" -> Success '123'
+-- parseString base10Integer' mempty "abc"     ->
+-- Failure (interactive):1:1: error: expected: parseDigit; abc<EOF>
+base10Integer' :: Parser Integer
+base10Integer' = do
+  neg <- optional (char '-')
+  xs  <- some parseDigit
+  case neg of
+    Nothing -> return $ read xs
+    Just x  -> return $ read $ x:xs
