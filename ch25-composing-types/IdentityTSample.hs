3c2d62ac5b752dc772494fda345c602be7630df2
u0164744 <giovanni.cosmiano@thomsonreuters.com>
2019-01-11 11:18:20 -0500

    ch25 - composing types - identityT sample

--- ch25-composing-types/IdentityTSample.hs
+++ ch25-composing-types/IdentityTSample.hs
@@ -54,4 +54,17 @@ instance (Monad m) => Monad (IdentityT m) where
 --     in  IdentityT aimb
 
 -----------------------------------------------------------------------------------
+-- |
+-- (>>=) :: m a -> (a -> m b) -> m b
+--
+-- The trick is we need `runIdentityT` because
+-- 1) 𝑓 returns IdentityT m b, but the
+-- 2) >>= for the Monad m => has the type `m a -> (a -> m b) -> m b`
+--
+-- It’ll end up trying to `join m (IdentityT m b)`, which won’t work because
+-- `m and IdentityT m` are not the same type. We use `runIdentityT` to unpack the value.
+-- Doing this  has the type `IdentityT m b -> m b` and the composition `runIdentityT . f`
+-- in this context has the type `a -> m b`.
+
+-----------------------------------------------------------------------------------
 
