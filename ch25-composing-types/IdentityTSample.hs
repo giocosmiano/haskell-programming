2fe8e1b4e1e1795582c36fa851e8411b34bbfa13
u0164744 <giocosmiano@gmail.com>
2019-01-11 16:03:43 -0500

    ch25 - composing types - identityT sample

--- ch25-composing-types/IdentityTSample.hs
+++ ch25-composing-types/IdentityTSample.hs
@@ -3,7 +3,12 @@
 module IdentityTSample where
 
 import Control.Monad
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 
+-----------------------------------------------------------------------------------
+-- | Identity vs IdentityT
 -----------------------------------------------------------------------------------
 
 -- |
@@ -13,16 +18,20 @@ newtype Identity a = Identity { runIdentity :: a } deriving (Eq, Show)
 
 -- |
 -- The identity monad transformer, serving only to specify that additional structure should exist.
-newtype IdentityT f a = IdentityT { runIdentityT :: f a } deriving (Eq, Show)
+newtype IdentityT m a = IdentityT { runIdentityT :: m a } deriving (Eq, Show)
 
+-----------------------------------------------------------------------------------
+-- | Functor implementations
 -----------------------------------------------------------------------------------
 
 instance Functor Identity where
   fmap f (Identity a) = Identity (f a)
 
 instance (Functor m) => Functor (IdentityT m) where
-  fmap f (IdentityT fa) = IdentityT (fmap f fa)
+  fmap f (IdentityT ma) = IdentityT (fmap f ma)
 
+-----------------------------------------------------------------------------------
+-- | Applicative implementations
 -----------------------------------------------------------------------------------
 
 instance Applicative Identity where
@@ -31,8 +40,10 @@ instance Applicative Identity where
 
 instance (Applicative m) => Applicative (IdentityT m) where
   pure x = IdentityT (pure x)
-  (IdentityT fab) <*> (IdentityT fa) = IdentityT (fab <*> fa)
+  (IdentityT mab) <*> (IdentityT ma) = IdentityT (mab <*> ma)
 
+-----------------------------------------------------------------------------------
+-- | Monad implementations
 -----------------------------------------------------------------------------------
 
 instance Monad Identity where
@@ -77,6 +88,75 @@ instance (Monad m) => Monad (IdentityT m) where
 -- `m and IdentityT m` are not the same type. We use `runIdentityT` to unpack the value.
 -- Doing this  has the type `IdentityT m b -> m b` and the composition `runIdentityT . f`
 -- in this context has the type `a -> m b`.
+--
 
 -----------------------------------------------------------------------------------
+-- | Foldable implementations
+-----------------------------------------------------------------------------------
+
+instance Foldable Identity where
+  foldMap f (Identity a) = f a
+  foldr f z (Identity a) = f a z
+
+instance (Foldable fa) => Foldable (IdentityT fa) where
+  foldMap f (IdentityT fa) = foldMap f fa
+  foldr f z (IdentityT fa) = foldr f z fa
+
+-----------------------------------------------------------------------------------
+-- | Traversable implementations
+-----------------------------------------------------------------------------------
+
+instance Traversable Identity where
+  traverse f (Identity a) = Identity <$> f a
+
+instance (Traversable fa) => Traversable (IdentityT fa) where
+  traverse f (IdentityT fa) = IdentityT <$> traverse f fa
+
+-----------------------------------------------------------------------------------
+
+instance (Eq a) => EqProp (Identity a) where (=-=) = eq
+
+instance (Eq (fa a)) => EqProp (IdentityT fa a) where (=-=) = eq
+
+instance (Arbitrary a) => Arbitrary (Identity a) where
+  arbitrary = do
+    a  <- arbitrary
+    return $ Identity a
+
+instance (Arbitrary (fa a), CoArbitrary (fa a)) => Arbitrary (IdentityT fa a) where
+  arbitrary = do
+    fa <- arbitrary
+    return $ IdentityT fa
+
+-----------------------------------------------------------------------------------
+
+-- search --> haskell applicative function checkers
+-- https://stackoverflow.com/questions/36009335/how-do-i-test-this-applicative-instance-with-checkers-no-instance-for-coarbitr
+
+-- https://begriffs.com/posts/2017-01-14-design-use-quickcheck.html
+
+-- https://hackage.haskell.org/package/checkers
+-- https://hackage.haskell.org/package/checkers-0.4.11/docs/Test-QuickCheck-Classes.html
+
+-- https://github.com/conal/checkers
+-- https://github.com/conal/checkers/blob/master/src/Test/QuickCheck/Classes.hs
+
+main = do
+
+  putStrLn "\nTesting Functor, Applicative, Monad, Traversable : Identity"
+  quickBatch $ functor (undefined :: Identity (Int, Double, Char))
+  quickBatch $ applicative (undefined :: Identity (Int, Double, Char))
+  quickBatch $ monad (undefined :: Identity (Int, Double, Char))
+  quickBatch $ traversable (undefined :: Identity (Int, Double, [Int]))
+
+  putStrLn "\nTesting Functor, Applicative, Monad, Traversable : IdentityT"
+  quickBatch $ functor (undefined :: IdentityT Maybe (Int, Double, Char))
+  quickBatch $ applicative (undefined :: IdentityT Maybe (Int, Double, Char))
+  quickBatch $ monad (undefined :: IdentityT Maybe (Int, Double, Char))
+  quickBatch $ traversable (undefined :: IdentityT Maybe (Int, Double, [Int]))
+
+-----------------------------------------------------------------------------------
+
+
+
 
