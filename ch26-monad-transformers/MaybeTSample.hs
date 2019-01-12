7a65dff06de19fc00747a95d70cb34f44491a63e
u0164744 <giocosmiano@gmail.com>
2019-01-11 21:13:14 -0500

    ch26 - monad transformers - maybeT sample

--- ch26-monad-transformers/MaybeTSample.hs
+++ ch26-monad-transformers/MaybeTSample.hs
@@ -31,24 +31,6 @@ instance (Monad m) => Monad (MaybeT m) where
         Nothing -> return Nothing
         Just x  -> runMaybeT (f x)
 
--- | OR ***
---   (>>=) :: MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
---   (MaybeT ma) >>= f =
---     let aimb = join (fmap runMaybeT (fmap f ma))
---     in  MaybeT aimb
-
--- | OR ***
---   (>>=) :: MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
---   (MaybeT ma) >>= f =
---     let aimb = join (fmap (runMaybeT . f) ma)
---     in  MaybeT aimb
-
--- | OR ***
---   (>>=) :: MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
---   m >>= k =
---     MaybeT $ runMaybeT . k
---     =<< runMaybeT m
-
 -----------------------------------------------------------------------------------
 
 instance (Foldable m) => Foldable (MaybeT m) where
@@ -58,40 +40,6 @@ instance (Traversable m) => Traversable (MaybeT m) where
   traverse f (MaybeT ma) = MaybeT <$> (traverse . traverse) f ma
 
 -----------------------------------------------------------------------------------
-
-{-
-
-instance (Eq (ma a)) => EqProp (MaybeT ma a) where (=-=) = eq
-
-instance (Arbitrary (ma a), CoArbitrary (ma a)) => Arbitrary (MaybeT ma a) where
-  arbitrary = do
-    ma <- arbitrary
-    return $ MaybeT ma
-
---newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
-
------------------------------------------------------------------------------------
-
--- search --> haskell applicative function checkers
--- https://stackoverflow.com/questions/36009335/how-do-i-test-this-applicative-instance-with-checkers-no-instance-for-coarbitr
-
--- https://begriffs.com/posts/2017-01-14-design-use-quickcheck.html
-
--- https://hackage.haskell.org/package/checkers
--- https://hackage.haskell.org/package/checkers-0.4.11/docs/Test-QuickCheck-Classes.html
-
--- https://github.com/conal/checkers
--- https://github.com/conal/checkers/blob/master/src/Test/QuickCheck/Classes.hs
-
-main = do
-
-  putStrLn "\nTesting Functor, Applicative, Monad, Traversable : MaybeT"
-  quickBatch $ functor (undefined :: MaybeT Maybe (Int, Double, Char))
-  quickBatch $ applicative (undefined :: MaybeT Maybe (Int, Double, Char))
-  quickBatch $ monad (undefined :: MaybeT Maybe (Int, Double, Char))
-  quickBatch $ traversable (undefined :: MaybeT Maybe (Int, Double, [Int]))
-
--}
 
 
 
