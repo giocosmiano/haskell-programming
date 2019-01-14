b3f9b11410f203152e7a87ee3e518da90a963f61
u0164744 <giovanni.cosmiano@thomsonreuters.com>
2019-01-14 15:30:00 -0500

    ch26 - monad transformers - maybeT exercises, adding some samples

--- ch26-monad-transformers/MaybeTExercises.hs
+++ ch26-monad-transformers/MaybeTExercises.hs
@@ -8,6 +8,8 @@ newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
 
 -----------------------------------------------------------------------------------
 
+-- e.g.
+-- runMaybeT $ (*3) <$> MaybeT [Just 3, Just 5, Just 7] -> [Just 9,Just 15,Just 21]
 instance (Functor m) => Functor (MaybeT m) where
 
   fmap :: (a -> b) -> MaybeT m a -> MaybeT m b
@@ -15,6 +17,9 @@ instance (Functor m) => Functor (MaybeT m) where
 
 -----------------------------------------------------------------------------------
 
+-- e.g.
+-- runMaybeT $ MaybeT [Just (*3), Just (+5)] <*> MaybeT [Just 5, Just 7]   -> [Just 15,Just 21,Just 10,Just 12]
+-- runMaybeT $ (*) <$> MaybeT [Just 2, Just 3] <*> MaybeT [Just 5, Just 7] -> [Just 10,Just 14,Just 15,Just 21]
 instance (Applicative m) => Applicative (MaybeT m) where
 
   pure :: Applicative m => a -> MaybeT m a
@@ -25,6 +30,9 @@ instance (Applicative m) => Applicative (MaybeT m) where
 
 -----------------------------------------------------------------------------------
 
+-- e.g.
+-- runMaybeT $ MaybeT [Just 5, Nothing, Just 7] >>= return . (*3)               -> [Just 15,Nothing,Just 21]
+-- runMaybeT $ MaybeT [Just 5, Nothing, Just 7] >>= return . \x -> [(x*5, x+3)] -> [Just [(25,8)],Nothing,Just [(35,10)]]
 instance (Monad m) => Monad (MaybeT m) where
   return = pure
 
@@ -38,9 +46,14 @@ instance (Monad m) => Monad (MaybeT m) where
 
 -----------------------------------------------------------------------------------
 
+-- e.g.
+-- foldMap Product (MaybeT [Just 5, Nothing, Just 7])                            -> Product {getProduct = 35}
+-- foldMap (*3) (MaybeT [Just (5::Sum Integer), Nothing, Just (7::Sum Integer)]) -> Sum {getSum = 36}
 instance (Foldable m) => Foldable (MaybeT m) where
   foldMap :: (Monoid mn, Foldable m) => (a -> mn) -> MaybeT m a -> mn
   foldMap f (MaybeT ma) = (foldMap . foldMap) f ma
+
+-----------------------------------------------------------------------------------
 
 instance (Traversable m) => Traversable (MaybeT m) where
   traverse :: Applicative fa => (a -> fa b) -> MaybeT m a -> fa (MaybeT m b)
