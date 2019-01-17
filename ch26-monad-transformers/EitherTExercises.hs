bf3bc0aad750e7299d471d356a96107eeb1b9d76
u0164744 <giocosmiano@gmail.com>
2019-01-16 19:02:25 -0500

    ch26 - monad transformers - eitherT exercises, adding some samples

--- ch26-monad-transformers/EitherTExercises.hs
+++ ch26-monad-transformers/EitherTExercises.hs
@@ -4,10 +4,22 @@ module EitherTExercises where
 
 -----------------------------------------------------------------------------------
 
+-- |
+-- e.g.
+-- import Data.Monoid
+-- runEitherT $ return 1                     -> Right 1
+-- runEitherT $ EitherT $ [Right 1]          -> [Right 1]
+-- runEitherT $ EitherT $ Right $ Right 1    -> Right (Right 1)
+-- runEitherT $ EitherT $ Identity $ Right 1 -> Identity (Right 1)
+--
+-- runEitherT $ EitherT $ [Left 1]           -> [Left 1]
+-- runEitherT $ EitherT $ Identity $ Left 1  -> Identity (Left 1)
+
 newtype EitherT e m a = EitherT { runEitherT :: m (Either e a) }
 
 -----------------------------------------------------------------------------------
 
+-- |
 -- e.g.
 -- runEitherT $ (*3) <$> EitherT [Right 3, Left 5, Right 7] -> [Right 9,Left 5,Right 21]
 instance (Functor m) => Functor (EitherT e m) where
@@ -17,6 +29,7 @@ instance (Functor m) => Functor (EitherT e m) where
 
 -----------------------------------------------------------------------------------
 
+-- |
 -- e.g.
 -- runEitherT $ EitherT [Right (*3)] <*> EitherT [Right 5, Left 7]              -> [Right 15,Left 7]
 -- runEitherT $ (*) <$> EitherT [Right 2, Left 3] <*> EitherT [Right 5, Left 7] -> [Right 10,Left 7,Left 3,Left 3]
@@ -30,6 +43,7 @@ instance (Applicative m) => Applicative (EitherT e m) where
 
 -----------------------------------------------------------------------------------
 
+-- |
 -- e.g.
 -- runEitherT $ EitherT [Right 5, Left 7] >>= return . (*3)               -> [Right 15,Left 7]
 -- runEitherT $ EitherT [Right 5, Left 7] >>= return . \x -> [(x*5, x+3)] -> [Right [(25,8)],Left 7]
@@ -46,6 +60,7 @@ instance (Monad m) => Monad (EitherT e m) where
 
 -----------------------------------------------------------------------------------
 
+-- |
 -- e.g.
 -- import Data.Monoid
 -- foldMap Product (EitherT [Right 5, Left 6, Right 7])                                           -> Product {getProduct = 35}
@@ -66,6 +81,7 @@ swapEither :: Either e a -> Either a e
 swapEither (Left  e) = Right e
 swapEither (Right a) = Left  a
 
+-- |
 -- transformer version of swapEither.
 swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
 swapEitherT (EitherT ma) = EitherT $ fmap swapEither $ runEitherT (EitherT ma)
