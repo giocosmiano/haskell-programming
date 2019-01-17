b482f0f453c90a6dc5fda1b46294e5d44861e4bb
u0164744 <giocosmiano@gmail.com>
2019-01-16 19:41:24 -0500

    ch23 - state - state exercise, adding some samples

--- ch23-state/MyStateExercise.hs
+++ ch23-state/MyStateExercise.hs
@@ -8,6 +8,14 @@ import Test.QuickCheck.Classes
 
 -----------------------------------------------------------------------------------
 
+-- |
+-- e.g.
+-- import Data.Monoid
+-- (runMoi $ Moi (\s -> (1, s))) 7              -> (1,7)
+-- (runMoi $ Moi (\s -> (1, s))) $ [7]          -> (1,[7])
+-- (runMoi $ Moi (\s -> (1, s))) $ Just 7       -> (1,Just 7)
+-- (runMoi $ Moi (\s -> (1, s))) $ Identity 7   -> (1,Identity 7)
+
 newtype Moi s a = Moi { runMoi :: s -> (a, s) }
 
 -----------------------------------------------------------------------------------
@@ -17,9 +25,14 @@ newtype Moi s a = Moi { runMoi :: s -> (a, s) }
 -- 2) Then, apply the current computation/function `f` to value `a`, resulting to `(b, s')`,
 --    which will become the argument to `Moi $`
 
+-- |
 -- e.g.
--- runMoi ((+3) <$> (Moi $ \s -> (5, s))) 7 -> (8,7)
--- runMoi ((*3) <$> (Moi $ \s -> (5, s))) 7 -> (15,7)
+-- import Data.Monoid
+-- runMoi ((+3) <$> (Moi $ \s -> (5, s))) 7            -> (8,7)
+-- runMoi ((*3) <$> (Moi $ \s -> (5, s))) 7            -> (15,7)
+-- runMoi ((+3) <$> (Moi $ \s -> (5, s))) $ Nothing    -> (8,Nothing)
+-- runMoi ((+3) <$> (Moi $ \s -> (5, s))) $ Just 7     -> (8,Just 7)
+-- runMoi ((*3) <$> (Moi $ \s -> (5, s))) $ Identity 7 -> (15,Identity 7)
 instance Functor (Moi s) where
 
   fmap :: (a -> b) -> Moi s a -> Moi s  b
@@ -35,9 +48,14 @@ instance Functor (Moi s) where
 -- 3) Finally, apply the new computation/function `f'` to value `a'`, resulting to `(b', s'')`,
 --    which will become the argument to `Moi $`
 
+-- |
 -- e.g.
--- runMoi ((Moi $ \s -> ((+3), s)) <*> (Moi $ \s -> (5, s))) 7      -> (8,7)
--- runMoi ((*) <$> (Moi $ \s -> (3, s)) <*> (Moi $ \s -> (5, s))) 7 -> (15,7)
+-- import Data.Monoid
+-- runMoi ((Moi $ \s -> ((+3), s)) <*> (Moi $ \s -> (5, s))) 7            -> (8,7)
+-- runMoi ((*) <$> (Moi $ \s -> (3, s)) <*> (Moi $ \s -> (5, s))) 7       -> (15,7)
+-- runMoi ((Moi $ \s -> ((+3), s)) <*> (Moi $ \s -> (5, s))) $ Nothing    -> (8,Nothing)
+-- runMoi ((Moi $ \s -> ((+3), s)) <*> (Moi $ \s -> (5, s))) $ Just 7     -> (8,Just 7)
+-- runMoi ((Moi $ \s -> ((*3), s)) <*> (Moi $ \s -> (5, s))) $ Identity 7 -> (15,Identity 7)
 instance Applicative (Moi s) where
 
   pure :: a -> Moi s a
@@ -57,9 +75,13 @@ instance Applicative (Moi s) where
 -- 3) Finally, unpack/extract function `s -> (b, s)` from `Moi s b` and apply to `newState`, resulting to `(b, newState)`,
 --    which will become the argument to `Moi $`
 
+-- |
 -- e.g.
--- runMoi ((Moi $ \s -> (5, s)) >>= return . (+3)) 7 -> (8,7)
--- runMoi ((Moi $ \s -> (5, s)) >>= return . (*3)) 7 -> (15,7)
+-- runMoi ((Moi $ \s -> (5, s)) >>= return . (+3)) 7            -> (8,7)
+-- runMoi ((Moi $ \s -> (5, s)) >>= return . (*3)) 7            -> (15,7)
+-- runMoi ((Moi $ \s -> (5, s)) >>= return . (+3)) $ Nothing    -> (8,Nothing)
+-- runMoi ((Moi $ \s -> (5, s)) >>= return . (+3)) $ Just 7     -> (8,Just 7)
+-- runMoi ((Moi $ \s -> (5, s)) >>= return . (*3)) $ Identity 7 -> (15,Identity 7)
 instance Monad (Moi s) where
   return = pure
 
@@ -68,6 +90,7 @@ instance Monad (Moi s) where
     let (a, newState) = f s
     in  (runMoi (g a)) newState
 
+-- |
 -- OR
 --  (Moi f) >>= g = Moi $ \s ->
 --    let (a, newState) = f s -- apply first/previous stateful computation `f` to `s` creating tuple with `newState`
@@ -76,23 +99,27 @@ instance Monad (Moi s) where
 
 -----------------------------------------------------------------------------------
 --
+-- |
 -- for further reading
 -- https://www.schoolofhaskell.com/school/starting-with-haskell/basics-of-haskell/12-State-Monad
 -- http://brandon.si/code/the-state-monad-a-tutorial-for-the-confused/
 -- http://dev.stephendiehl.com/hask/
 
+-- |
 -- `get` is when we want to construct a `State`, feeding some `s` to State returning `(s, s)`
 -- e.g.
 -- runMoi get "curryIsAmaze" -> ("curryIsAmaze","curryIsAmaze")
 get :: Moi s s
 get = Moi $ \s -> (s, s)
 
+-- |
 -- `put` is when we want to construct a `State`, feeding some `s` to State returning `((), s)`
 -- e.g.
 -- runMoi (put "blah") "woot" -> ((),"blah")
 put :: s -> Moi s ()
 put s = Moi $ \_ -> ((), s)
 
+-- |
 -- `exec` is when we want the `state` value and not the `a` value
 -- e.g.
 -- exec (put "wilma") "daphne" -> "wilma"
@@ -100,6 +127,7 @@ put s = Moi $ \_ -> ((), s)
 exec :: Moi s a -> s -> s
 exec sa s = snd $ runMoi sa s
 
+-- |
 -- `eval` is when we want the `a` value and not the `state` value
 -- e.g.
 -- eval get "bunnicula"     -> "bunnicula"
@@ -107,6 +135,7 @@ exec sa s = snd $ runMoi sa s
 eval :: Moi s a -> s -> a
 eval sa s = fst $ runMoi sa s
 
+-- |
 -- `modify` is when we want to construct a `State` with function to modifying the state,
 -- feeding some `s` to State returning `((), fn s)`
 -- e.g.
