5a831979c1f087dc41af090bad90eef1154fd791
u0164744 <giovanni.cosmiano@thomsonreuters.com>
2019-01-18 13:48:18 -0500

    ch26 - monad transformers - lift maybeT vs maybeT

--- ch26-monad-transformers/LiftMaybeTExercises.hs
+++ ch26-monad-transformers/LiftMaybeTExercises.hs
@@ -3,33 +3,55 @@
 module LiftMaybeTExercises where
 
 import Data.Functor.Identity
-import Control.Monad.Trans.Maybe
+import Control.Monad
 import Control.Monad.Trans.Class
 
 -----------------------------------------------------------------------------------
 -- Using `lift` from Control.Monad.Trans.Class
 -----------------------------------------------------------------------------------
-
-{-
-
-class MonadTrans t where
-
--- | Lift a computation from the argument monad to the constructed monad.
-  lift :: (Monad m) => m a -> t m a
-
 -- |
+-- Lift a computation from the argument monad to the constructed monad.
+--
 -- Here the `t` is a (constructed) monad transformer type that has an instance of MonadTrans defined.
+--
+-- class MonadTrans t where
+--   lift :: (Monad m) => m a -> t m a
 
--}
+-----------------------------------------------------------------------------------
+
+newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
+
+instance MonadTrans MaybeT where
+  lift = MaybeT . liftM Just
 
 -----------------------------------------------------------------------------------
 -- |
--- newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
+-- lift :: (Monad m) => m a -> t m a
+-- (MaybeT . liftM Just) :: Monad m => m a -> MaybeT m a
+--
+-- MaybeT :: m (Maybe a) -> MaybeT m a
+-- (liftM Just) :: Monad m => m a -> m (Maybe a)
+--
+-- v :: Monad m => m a
+-- liftM Just :: Monad m => m a -> m (Maybe a)
+-- liftM Just v :: m (Maybe a)
+-- MaybeT (liftM Just v) :: MaybeT m a
+--
 -----------------------------------------------------------------------------------
+-- Roughly speaking, this has taken an m a and lifted it into a MaybeT context
+--
+-- The general pattern with MonadTrans instances demonstrated by MaybeT is that you’re usually
+-- going to lift the injection of the known structure (with MaybeT, the known structure is Maybe)
+-- over some Monad. Injection of structure usually means return, but since with MaybeT we know we
+-- want Maybe structure, we use Just. That transforms an m a into m (T a) where capital T is some
+-- concrete type you’re lifting the m a into. Then to cap it all off, you use the data constructor
+-- for your monad transformer, and the value is now lifted into the larger context.
 
+-----------------------------------------------------------------------------------
 -- |
 -- e.g.
 -- runMaybeT $ return 1                   -> Just 1
+-- runMaybeT $ MaybeT $ Just $ return 1   -> Just (Just 1)
 -- runMaybeT $ MaybeT [Just 1]            -> [Just 1]
 -- runMaybeT $ MaybeT $ Just $ Just 1     -> Just (Just 1)
 -- runMaybeT $ MaybeT $ Identity $ Just 1 -> Identity (Just 1)
@@ -37,6 +59,7 @@ class MonadTrans t where
 -- |
 -- e.g.
 -- runMaybeT . lift $ return 1          -> Just 1
+-- runMaybeT . lift $ Just 1            -> Just (Just 1)
 -- runMaybeT . lift $ [Just 1]          -> [Just (Just 1)]
 -- runMaybeT . lift $ Just $ Just 1     -> Just (Just (Just 1))
 -- runMaybeT . lift $ Identity $ Just 1 -> Identity (Just (Just 1))
