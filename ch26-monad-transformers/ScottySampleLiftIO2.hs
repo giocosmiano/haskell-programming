fb08265f3ebe19c069a22bcf3d42cd20db3069fe
u0164744 <giocosmiano@gmail.com>
2019-01-18 22:06:41 -0500

    ch26 - monad transformers - scotty using liftIO 2

---
+++ ch26-monad-transformers/ScottySampleLiftIO2.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE InstanceSigs      #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module ScottySampleLiftIO2 where
+
+import Web.Scotty
+
+import Control.Monad.IO.Class
+import Data.Maybe (fromMaybe)
+import Data.Monoid (mconcat)
+import Data.Text.Lazy (Text)
+
+-----------------------------------------------------------------------------------
+-- |
+-- e.g.
+-- curl -X GET http://localhost:3000/beam/         -> Nothing
+-- curl -X GET http://localhost:3000/beam?num=123  -> Just 123
+
+param' :: Parsable a => Text -> ActionM (Maybe a)
+param' k = rescue (Just <$> param k)
+  (const (return Nothing))
+
+main :: IO ()
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam' <- param' "word"
+    let beam = fromMaybe "" beam'
+    i <- param' "num"
+    liftIO $ print (i :: Maybe Integer)
+    html $ mconcat
+      [ "<h1>Scotty, "
+      , beam
+      , " me up!</h1>" ]
+
+-----------------------------------------------------------------------------------
+
+
+
