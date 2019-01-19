031a15b98d384aa5db29a828911432054debc491
u0164744 <giocosmiano@gmail.com>
2019-01-18 22:23:38 -0500

    ch26 - monad transformers - scotty using liftIO 2

--- ch26-monad-transformers/ScottySampleLiftIO2.hs
+++ ch26-monad-transformers/ScottySampleLiftIO2.hs
@@ -15,6 +15,14 @@ import Data.Text.Lazy (Text)
 -- e.g.
 -- curl -X GET http://localhost:3000/beam/         -> Nothing
 -- curl -X GET http://localhost:3000/beam?num=123  -> Just 123
+--
+-- Prelude> :t param
+-- param :: Parsable a => Data.Text.Internal.Lazy.Text -> ActionM a
+--
+-- Prelude> :t rescue
+-- rescue
+--   :: ActionM a
+--      -> (Data.Text.Internal.Lazy.Text -> ActionM a) -> ActionM a
 
 param' :: Parsable a => Text -> ActionM (Maybe a)
 param' k = rescue (Just <$> param k)
