5cf2741f65e7b9a33fdf4771a7e74708412931dc
u0164744 <giovanni.cosmiano@thomsonreuters.com>
2019-01-31 15:06:45 -0500

    ch28 - basic libraries - benchmarking DList sample

--- ch28-basic-libraries/BenchmarkingDList.hs
+++ ch28-basic-libraries/BenchmarkingDList.hs
@@ -19,16 +19,22 @@ toList :: DList a -> [a]
 toList = ($[]) . unDL
 {-# INLINE toList #-}
 
+-- |
+-- Prepend a single element to a dlist
 infixr `cons`
 cons :: a -> DList a -> DList a
 cons x xs = DL ((x:) . unDL xs)
 {-# INLINE cons #-}
 
+-- |
+-- Append a single element to a dlist
 infixl `snoc`
 snoc :: DList a -> a -> DList a
 snoc xs x = DL (unDL xs . (x:))
 {-# INLINE snoc #-}
 
+-- |
+-- Append dlist
 append :: DList a -> DList a -> DList a
 append xs ys = DL (unDL xs . unDL ys)
 {-# INLINE append #-}
@@ -48,9 +54,9 @@ constructDList i = toList $ go i empty
 main :: IO ()
 main = defaultMain
   [ bench "concat list" $
-    whnf schlemiel 123456
+    whnf schlemiel 1234567
   , bench "concat dlist" $
-    whnf constructDList 123456
+    whnf constructDList 1234567
   ]
 
 -----------------------------------------------------------------------------------
