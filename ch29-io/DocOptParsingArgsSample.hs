75e914fce21b41cea2aac50f6e073a0d5542a593
u0164744 <giocosmiano@gmail.com>
2019-02-03 10:32:55 -0500

    ch29 - io - docopt command-line interface parser sample

---
+++ ch29-io/DocOptParsingArgsSample.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module DocOptParsingArgsSample where
+
+import Control.Monad (when)
+import Data.Char (toUpper)
+import System.Environment (getArgs)
+import System.Console.Docopt
+
+-----------------------------------------------------------------------------------
+
+patterns :: Docopt
+patterns = [docoptFile|DOCOPT_SAMPLE_USAGE.txt|]
+
+getArgOrExit = getArgOrExitWith patterns
+
+-----------------------------------------------------------------------------------
+
+main = do
+  args <- parseArgsOrExit patterns =<< getArgs
+
+  when (args `isPresent` (command "cat")) $ do
+    file <- args `getArgOrExit` (argument "file")
+    putStr =<< readFile file
+
+  when (args `isPresent` (command "echo")) $ do
+    let charTransform = if args `isPresent` (longOption "caps")
+                          then toUpper
+                          else id
+    string <- args `getArgOrExit` (argument "string")
+    putStrLn $ map charTransform string
+
+-- |
+-- A command-line interface parser that will make you smile
+-- https://hackage.haskell.org/package/docopt
+-- https://github.com/docopt/docopt.hs
+--
+-- $ stack install doctopt
+--
+-- $ stack ParsingArgsSample.hs echo "hello world"
+-- hello world
+--
+-- $ stack ParsingArgsSample.hs echo -c "hello world"
+-- HELLO WORLD
+--
+-- $ stack DocOptParsingArgsSample.hs cat ./readme.md
+
+-----------------------------------------------------------------------------------
