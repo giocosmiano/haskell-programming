048b1c53e4bfe3b66bd3aabb628663b3f4ba2716
u0164744 <giovanni.cosmiano@thomsonreuters.com>
2019-02-05 14:13:25 -0500

    ch30 - exceptions - samples

---
+++ ch30-when-things-go-wrong/WhySomeException.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE GADTs                     #-}
+
+module WhySomeException where
+
+import Control.Exception
+  ( ArithException(..)
+  , AsyncException(..))
+
+import Data.Typeable
+
+-----------------------------------------------------------------------------------
+
+data MyException =
+  forall e . (Show e, Typeable e) => MyException e
+
+instance Show MyException where
+  showsPrec p (MyException e) = showsPrec p e
+
+-----------------------------------------------------------------------------------
+
+multiError :: Int -> Either MyException Int
+multiError n =
+  case n of
+    0 -> Left (MyException DivideByZero)
+    1 -> Left (MyException StackOverflow)
+    _ -> Right n
+
+-----------------------------------------------------------------------------------
+
+data SomeError =
+    Arith ArithException
+  | Async AsyncException
+  | SomethingElse
+  deriving (Show)
+
+discriminateError :: MyException -> SomeError
+discriminateError (MyException e) =
+  case cast e of
+    (Just arith) -> Arith arith
+    Nothing      ->
+      case cast e of
+        (Just async) -> Async async
+        Nothing      -> SomethingElse
+
+-----------------------------------------------------------------------------------
+
+runDisc n =
+  either discriminateError
+  (const SomethingElse) (multiError n)
+
+-- |
+-- Prelude> runDisc 0
+-- Arith divide by zero
+--
+-- Prelude> runDisc 1
+-- Async stack overflow
+--
+-- Prelude> runDisc 2
+-- SomethingElse
+--
+
+-----------------------------------------------------------------------------------
+
+
+
+
