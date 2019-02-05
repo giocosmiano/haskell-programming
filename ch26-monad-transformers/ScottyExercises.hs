ef10e38cd51816716fd68cc46d3da8f6220542d7
u0164744 <giovanni.cosmiano@thomsonreuters.com>
2019-02-05 16:13:02 -0500

    ch26 - monad transformers - scotty exercises - WIP

--- ch26-monad-transformers/ScottyExercises.hs
+++ ch26-monad-transformers/ScottyExercises.hs
@@ -3,6 +3,7 @@
 
 module ScottyExercises where
 
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
 import Data.IORef
@@ -49,23 +50,39 @@ type Handler = ActionT Text (ReaderT Config IO)
 
 -----------------------------------------------------------------------------------
 
+-- |
+-- Data.Map samples
+-- https://www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/Simple%20examples#data-map
+
 bumpBoomp :: Text -> M.Map Text Integer -> (M.Map Text Integer, Integer)
-bumpBoomp k m = undefined
+bumpBoomp k m = do
+  m' <-
+    case M.member k m of
+      True  -> return $ M.adjust (+1) k m
+      False -> return $ M.insert k 1 m
+  let count = M.findWithDefault 0 k m'
+  (m', count)
 
 -----------------------------------------------------------------------------------
 
+-- |
+-- Data.IORef samples
+-- https://www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/Simple%20examples#data-ioref
+
 app :: Scotty ()
-app = undefined
---app =
---  get "/:key" $ do
---    unPrefixed <- param "key"
---    let key' = mappend undefined unPrefixed
---    newInteger <- undefined
---    html $ mconcat
---      [ "<h1>Success! Count was: "
---      , TL.pack $ show newInteger
---      , "</h1>"
---      ]
+app =
+  get "/:key" $ do
+    unPrefixed <- param "key"
+    config <- lift ask
+    let key' = mappend (prefix config) unPrefixed
+        map' = readIORef . counts $ config
+    (newMap, newInteger) <- liftIO (bumpBoomp key' <$> map')
+    liftIO $ writeIORef (counts config) newMap
+    html $ mconcat
+      [ "<h1>Success! Count was: "
+      , TL.pack $ show newInteger
+      , "</h1>"
+      ]
 
 -----------------------------------------------------------------------------------
 
@@ -99,13 +116,20 @@ main = do
 -----------------------------------------------------------------------------------
 
 -- |
---
 -- e.g.
+-- Prelude> main lol
+-- Setting phasers to stun... (port 3000) (ctrl-c to quit)
+--
 -- $ curl localhost:3000/woot
 -- <h1>Success! Count was: 1</h1>
 -- $ curl localhost:3000/woot
 -- <h1>Success! Count was: 2</h1>
 -- $ curl localhost:3000/blah
 -- <h1>Success! Count was: 1</h1>
+--
+-- *** NOTE ***
+-- The underlying “key” used in the counter when we GET /woot is "lolwoot"
+-- because we passed ”lol” to main.
 
+-----------------------------------------------------------------------------------
 
